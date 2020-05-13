2324f904ea812de100bfe35dc375d0bdb9a8e141
jdjin3000 <57910677+jdjin3000@users.noreply.github.com>
2020-05-13 17:25:31 +0900

    Update classifier.py

--- ---classifierAPIServer---/classifier.py
+++ ---classifierAPIServer---/classifier.py
@@ -3,7 +3,8 @@ from tensorflow.keras.models import load_model
 import numpy as np
 import cv2, os
 
-model = load_model('models/MU/model.h5')
+WU_model = load_model('models/WU/model.h5')
+WL_model = load_model('models/WL/model.h5')
 app = Flask (__name__)
 
 @app.route('/classify', methods = ['POST'])
@@ -11,21 +12,32 @@ def classify():
     json_data = request.get_json(force=True) #json 데이터를 받음. cropped 내부에 있을 파일명만 전송해주면 됨.
     filename = json_data['filename']
     filename = os.path.join('../cropped/', filename+'.jpg')
+    
     MU_classDict = {0: 'Blazer', 1: 'Cardigan', 2: 'Coat', 3: 'Flannel', 4: 'Hoodie', 5: 'Jacket', 6: 'Parka', 7: 'Sweater', 8: 'Tee', 9: 'Turtleneck'}
-
+    
+    WU_classDict = {0: 'Blazer', 1: 'Blouse', 2: 'Cardigan', 3: 'Coat', 4: 'Dress', 5: 'Flannel', 6: 'Hoodie', 7: 'Jacket', 8: 'Parka', 9: 'Poncho', 10: 'Shirts', 11: 'Sweater', 12: 'Tank', 13: 'Top', 14: 'Turtleneck'}
+    WL_classDict = {0: 'Culottes', 1: 'Cutoffs', 2: 'Jeans', 3: 'Jeggings', 4: 'Joggers', 5: 'Leggings', 6: 'Shorts', 7: 'Skirt'}
+    
     image = cv2.resize(cv2.imread(filename),(299,299))
     image = np.array([image])/255
-    pred = model.predict(image)[0]
+    WU_pred = WU_model.predict(image)[0]
+    WL_pred = WL_model.predict(image)[0]
 
-    top_values_index = sorted(pred)[-5:]
+    WU_top_values_index = sorted(WU_pred)[-5:]
+    WL_top_values_index = sorted(WL_pred)[-5:]
 
     result = []
-    for i in range(len(pred)):
-        if pred[i] in top_values_index:
-            result.append({'fashionClass': MU_classDict[i], 'percentage': round(float(pred[i]), 2)})
+    for i in range(len(WU_pred)):
+        if WU_pred[i] in WU_top_values_index:
+            result.append({'fashionClass': WU_classDict[i], 'percentage': round(float(WU_pred[i]), 2)})
 
-    print(result)
-    return jsonify(result)
+    for i in range(len(WL_pred)):
+        if WL_pred[i] in WL_top_values_index:
+            result.append({'fashionClass': WL_classDict[i], 'percentage': round(float(WL_pred[i]), 2)})
+
+    result = sorted(result, key=lambda x:x['percentage'])
+
+    return jsonify(result[-5:])
 
 if __name__ == "__main__":
     app.run()

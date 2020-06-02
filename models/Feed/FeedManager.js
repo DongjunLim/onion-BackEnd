e14a526cdf7db03021685e67d3e5e46ae89cf6cc
jdjin3000 <57910677+jdjin3000@users.noreply.github.com>
2020-06-02 14:49:22 +0900

    Update filter Condition

--- models/Feed/FeedManager.js
+++ models/Feed/FeedManager.js
@@ -184,10 +184,10 @@ class FeedManager{
 		feed_handler.author_age = age;
 		feed_handler.author_profile_photo = 'profile/' + userNickname;
 
-		if (DominantColor.filter(value => color.includes(value)) != []){
+		if (DominantColor.filter(value => color.includes(value)).length != 0){
 			feed_handler.IsCorrectColor = 'Y'
 		}
-		if (fashionClass.filter(value => category.includes(value)) != []){
+		if (fashionClass.filter(value => category.includes(value)).length != 0){
 			feed_handler.IsCorrectClass = 'Y'
 		}
 
